0e47e17f532bc91b4ff46dc8242b1ad91c3618aa
Solomon Mekuria <70961231+smekuria1@users.noreply.github.com>
2023-06-03 16:43:47 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterView } from "vue-router";
+import HeaderTodo from "./components/HeaderTodo.vue";
+</script>
+
+<template>
+  <HeaderTodo />
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Krub:ital,wght@1,300&family=Nunito+Sans:wght@200&family=Rubik:wght@300;500;700&display=swap");
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/TodoCreate.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="input-wrap" :class="{ 'input-err': todoState.invalid }">
+    <input
+      type="text"
+      placeholder="What needs to be done?"
+      v-model="todoState.todo"
+      @keyup.enter="createTodo"
+    />
+    <button @click="createTodo()">Create</button>
+  </div>
+  <p class="err-msg" v-if="todoState.invalid">{{ todoState.errorMsg }}</p>
+</template>
+
+<script setup>
+import { ref, defineEmits, reactive } from "vue";
+const todoState = reactive({
+  todo: "",
+  invalid: null,
+  errorMsg: "",
+});
+const emit = defineEmits(["createTodo"]);
+const createTodo = () => {
+  todoState.invalid = null;
+  todoState.errorMsg = "";
+  if (todoState.todo !== "") {
+    emit("createTodo", todoState.todo);
+    todoState.todo = "";
+    return;
+  }
+  todoState.invalid = true;
+  todoState.errorMsg = "Todo cannot be empty";
+};
+</script>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  &.input-err {
+    border-color: red;
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
+}
+</style>
